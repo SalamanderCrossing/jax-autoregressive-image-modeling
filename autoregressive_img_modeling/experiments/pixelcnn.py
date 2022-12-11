ae60ff165858932a487fe006ecc7640876634e7f
salamandercrossing <giulio.zani@gmail.com>
2022-12-11 13:47:43 +0000

    update

--- autoregressive_img_modeling/experiments/pixelcnn.py
+++ autoregressive_img_modeling/experiments/pixelcnn.py
@@ -14,7 +14,7 @@ trainer = TrainerModule(
     model, next(iter(train_loader))[0], mate.default_checkpoint_location, mate.save_dir
 )
 if mate.is_train:
-    _, result = trainer.train_model(train_loader, val_loader)
+    _, result = trainer.train_model(train_loader, val_loader, num_epochs=10)
     mate.result(result)
 elif mate.is_test:
     result = trainer.test_model(test_loader)
